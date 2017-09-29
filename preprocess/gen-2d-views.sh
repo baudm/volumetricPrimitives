c7f325f077b1bd8fff81fb93bb3bb87bffc9d792
Darwin Bautista <darwin.bautista@eee.upd.edu.ph>
2017-09-29 13:50:35 +0800

    Fix generation of 2D view for wrong 3D model

--- preprocess/gen-2d-views.sh
+++ preprocess/gen-2d-views.sh
@@ -10,15 +10,17 @@ IMAGE_DIR="../cachedir/shapenet/images/${SYNSET}"
 mkdir -p $IMAGE_DIR
 
 process() {
-	name="$(basename $1)"
+	path="$1"
+	name="$(basename $path)"
 	png="${IMAGE_DIR}/${name}.png"
 	echo $name
 	[ -f "$png" ] && continue
-	../renderer/render.sh `which blender` ../renderer/model.blend "${p}/model.obj" "$png"
+	../renderer/render.sh `which blender` ../renderer/model.blend "${path}/model.obj" "$png"
 	mogrify -crop 402x402+350+1 "$png"
 	convert "$png" -colorspace gray -average "$png"
 }
 
+# Parallel processing using 4 threads
 
 for p in ${SYNSET_PATH}/[0-4]*; do
 	process "$p"
@@ -32,6 +34,6 @@ for p in ${SYNSET_PATH}/[9abc]*; do
 	process "$p"
 done &
 
-for p in ${SYNSET_PATH}/[def]*; do
+for p in ${SYNSET_PATH}/[defu]*; do
 	process "$p"
 done
